f2ac72a70cc7e181557577f6f2714564fec5d281
Daisel10 <davidsanchezlombana10@gmail.com>
2024-05-18 18:41:45 -0500

    firts commit

---
+++ mai_test1.py
@@ -0,0 +1,47 @@
+import flet as ft
+import threading
+import time
+
+def main(page: ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    
+    
+    
+    c1 = ft.Container(
+        ft.Text("Hello!", style=ft.TextThemeStyle.HEADLINE_MEDIUM),
+        alignment=ft.alignment.center,
+        width=page.vertical_alignment,
+        height=page.height,
+        bgcolor=ft.colors.GREEN,
+    )
+    c2 = ft.Container(
+        ft.Text("Bye!", size=50),
+        alignment=ft.alignment.center,
+        width=page.vertical_alignment,
+        height=page.horizontal_alignment,
+        bgcolor=ft.colors.YELLOW,
+    )
+    c = ft.AnimatedSwitcher(
+        c1,
+        transition=ft.AnimatedSwitcherTransition.SCALE,
+        duration=800,
+        reverse_duration=200,
+        switch_in_curve=ft.AnimationCurve.BOUNCE_OUT,
+        switch_out_curve=ft.AnimationCurve.BOUNCE_IN,
+    )
+
+    def animate(e):
+        c.content = c2 if c.content == c1 else c1
+        c.update()
+
+    t = threading.Timer(2, animate)
+    
+    page.add(
+        c,
+        # ft.ElevatedButton("Animate!", on_click=animate),
+        t.start(),
+        
+    )
+
+ft.app(target=main)
