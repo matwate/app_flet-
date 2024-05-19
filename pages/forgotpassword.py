158f59a657e571475b85106a1256c16fc71bb776
Daisel10 <davidsanchezlombana10@gmail.com>
2024-05-18 19:51:42 -0500

    changes of the schema

--- pages/forgotpassword.py
+++
@@ -1,8 +0,0 @@
-from flet import *
-
-class ForgotPassword(UserControl):
-  def __init__(self):
-    super().__init__()
-
-  def build(self):
-    return Column()
--- pages/signup.py
+++
@@ -1,8 +0,0 @@
-from flet import *
-
-class Signup(UserControl):
-  def __init__(self):
-    super().__init__()
-
-  def build(self):
-    return Column()
